65b5bb6898dce4b9e3f6520b07375e03846ec83b
David Zaharee <dzaharee@vlacs.org>
2014-06-12 14:36:36 -0400

    make first attempt at section schema

--- src/oarlock/schema.clj
+++ src/oarlock/schema.clj
@@ -8,7 +8,6 @@
             [:id-sk-with-origin :string "Combined id-sk and id-sk-origin" :db.unique/identity]
             [:name :string]
             [:version :string] ; not null
-            ;; Example types: :project :course-competency ... TODO: enum?
             [:type :string] ; not null
             [:duration-rating-days :bigint]
             [:comps :ref :many]
@@ -23,16 +22,24 @@
             [:name :string]
             [:version :string]
             [:description :string]
-            ;; TODO: is this correct?
             [:comps :ref :many]]
     :dbfns [(constraints/unique :perf-asmt :id-sk :id-sk-origin)]}
+   {:namespace :section
+    :attrs [[:id-sk :string]
+            [:instructor :ref]
+            [:perf-asmts :ref :many]
+            [:status :enum [:active :archived :preactive]]]}
    {:namespace :user2perf-asmt
     :attrs [[:user :ref]
             [:perf-asmt :ref]
-            ;; TODO: finish this
             [:grade :boolean]]}
    {:namespace :user2task
     :attrs [[:user :ref]
             [:task :ref]
-            ;; TODO: finish this
-            [:progress :boolean]]}])
+            [:progress :boolean]]}
+   {:namespace :user2section
+    :attrs [[:user :ref]
+            [:section :ref]
+            ;; TODO: do we want to use our current model of status, or do something different?
+            [:status :enum [:active :completed :withdrawn-failing :withdrawn-no-grade]]
+            [:activation-status :enum [:assigned :assigned-emailed :enabled :contact-instructor]]]}])
