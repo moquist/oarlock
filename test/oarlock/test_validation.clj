39e6ccf1b92427e495e49e18bf341e415d136379
David Zaharee <dzaharee@vlacs.org>
2014-07-14 10:29:33 -0400

    fix broken test

--- test/oarlock/test_validation.clj
+++ test/oarlock/test_validation.clj
@@ -11,5 +11,5 @@
     (is (= (validator :perf-asmt {}) "Value does not match schema: {:version missing-required-key, :comps missing-required-key, :credit-value-numerator missing-required-key, :id-sk missing-required-key, :type missing-required-key, :duration-rating-days missing-required-key, :credit-value-denominator missing-required-key, :id-sk-origin missing-required-key}"))))
 
 (deftest student2perf-asmt-validation-test
-  (testing "user2perf-asmt validation"
-    (is (= (validator :student2perf-asmt {}) "Value does not match schema: {:grade missing-required-key, :task missing-required-key, :user missing-required-key}"))))
+  (testing "student2perf-asmt validation"
+    (is (= (validator :student2perf-asmt {}) "Value does not match schema: {:grade missing-required-key, :task missing-required-key, :user missing-required-key, :id-sk-origin missing-required-key, :id-sk missing-required-key}"))))
